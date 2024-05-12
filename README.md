17003f70ab73661f03f6b80a793026c073d358db
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-12 17:15:18 +0300

    chore: add publisher to JSR

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # @gramio/auto-retry
 
-A plugin that catches errors with the retry_after field (rate limit errors), waits for the specified time and repeats the API request.
+[![npm](https://img.shields.io/npm/v/@gramio/auto-retry?logo=npm&style=flat&labelColor=000&color=3b82f6)](https://www.npmjs.org/package/@gramio/auto-retry)
+[![JSR](https://jsr.io/badges/@gramio/auto-retry)](https://jsr.io/@gramio/auto-retry)
+[![JSR Score](https://jsr.io/badges/@gramio/auto-retry/score)](https://jsr.io/@gramio/auto-retry)
+
+A plugin that catches errors with the `retry_after` field (**rate limit** errors), **waits** for the specified time and **repeats** the API request.
 
 ```ts
 import { Bot } from "gramio";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import { Plugin } from "gramio";
 
-export function autoRetry() {
+/**
+ * A plugin that catches errors with the `retry_after` field (**rate limit** errors), **waits** for the specified time and **repeats** the API request.
+ * @example
+ * ```ts
+ * import { Bot } from "gramio";
+ * import { autoRetry } from "@gramio/auto-retry";
+ *
+ * const bot = new Bot(process.env.TOKEN!)
+ *     .extend(autoRetry())
+ *     .command("start", async (context) => {
+ *         for (let index = 0; index < 100; index++) {
+ *             await context.reply(`some ${index}`);
+ *         }
+ *     })
+ *     .onStart(console.log);
+ *
+ * bot.start();
+ * ```
+ */
+export function autoRetry(): Plugin {
 	return new Plugin("@gramio/auto-retry").onResponseError(
 		async (error, api) => {
 			if (error.payload?.retry_after) {
